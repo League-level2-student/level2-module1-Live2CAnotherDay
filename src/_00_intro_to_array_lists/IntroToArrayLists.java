612b3a3dc0dad1b13d663ad3ca3823b189499b05
league <league@imac-24.attlocal.net>
2023-12-02 16:29:15 -0800

    Finish the intro to array lists thing

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,14 +1,22 @@
 package _00_intro_to_array_lists;
 
+import _01_array_list_visualizer.data.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+		ArrayList<String> ppls = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		ppls.add("Boi");
+		ppls.add("Man");
+		ppls.add("Homie");
+		ppls.add("Walter");
+		ppls.add("Jesse");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i =0; i < ppls.size();i++ ) {
+			
+		}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
