a816aa4bd7626f2054ffc757d44acbf7348385ed
league <jacktu2718@gmail.com>
2023-12-16 16:29:09 -0800

    Start on the guest book

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,28 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
-		
+		list.add("Negan");
+		list.add("Atlanta");
+		list.add("France");
+		list.add("Rick");
+		list.add("Carl");
 		// 2. Set one item in our list to be something different
-		
+		list.set(0, "Dixon");
 		// 3. Remove something from our list
-		
+		list.remove(4);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if (list.contains("Negan") ) {
+			System.out.println("Rip Glenn");
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(0));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
@@ -58,3 +65,5 @@ public class VisualArrayLists implements ActionListener {
 		list.draw();
 	}
 }
+
+// Nothing else to do in here.
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -2,6 +2,7 @@ package _02_array_list_guestbook;
 
 public class GuestBook {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
